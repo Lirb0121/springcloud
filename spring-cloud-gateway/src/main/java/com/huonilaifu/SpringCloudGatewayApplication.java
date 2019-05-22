d9108f1a69d89b9f0e5764f97f4c6b98114d8a69
lirb <git@lirb>
2019-05-22 23:51:16 +0800

    'gateway'

---
+++ spring-cloud-gateway/src/main/java/com/huonilaifu/SpringCloudGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.huonilaifu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class SpringCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+	}
+
+}
